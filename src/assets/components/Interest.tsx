9646b7e96ed499e99ad3bf7504b184304d06cbca
Pratit Bangdiwala <pratit.bangdiwala7@gmail.com>
2025-12-29 12:41:53 +0530

    fix: cursor pointer for item

--- src/assets/components/Interest.tsx
+++ src/assets/components/Interest.tsx
@@ -3,7 +3,7 @@ import type { InterestProps } from "../utils/types"
 
 const Interest: React.FC<InterestProps> = ({ label, selected, getInterestFn }) => {
     return (
-        <div className={`bg-grayDrk rounded-xl flex items-center py-1 pr-4 pl-2 border ${selected ? 'border-green-500' : ' border-transparent'}`} onClick={getInterestFn}>
+        <div className={`bg-grayDrk rounded-xl flex items-center py-1 pr-4 pl-2 border cursor-pointer ${selected ? 'border-green-500' : ' border-transparent'}`} onClick={getInterestFn}>
             {selected ? <IoIosCheckmark color="#16a34a" size={28} /> : <IoIosAdd color="#ffffff" size={28} />}
             <p className={`text-grayLt capitalize ${selected ? 'text-green-300' : ''}`}>{label}</p>
         </div>
